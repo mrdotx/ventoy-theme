9df2ab8c2f8158e1eaff5c6b4be2faf187d2954a
mrdotx <klassiker@gmx.de>
2025-02-23 06:55:27 +0100

    colors and category security added

--- install.sh
+++ install.sh
@@ -3,32 +3,43 @@
 # path:   /home/klassiker/.local/share/repos/ventoy-theme/install.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/ventoy-theme
-# date:   2024-04-27T17:43:07+0200
+# date:   2025-02-23T06:54:25+0100
 
 # auth can be something like sudo -A, doas -- or nothing,
 # depending on configuration requirements
 auth="${EXEC_AS_USER:-sudo}"
 label="Ventoy"
 
+# color variables
+reset="\033[0m"
+bold="\033[1m"
+blue="\033[94m"
+cyan="\033[96m"
+
 # mount ventoy partition
 [ -h "/dev/disk/by-label/$label" ] \
     && mnt="/mnt/$label" \
-    && printf ":: create and mount ventoy folder %s\n" "$mnt" \
+    && printf "%b%b::%b %bCREATE%b and %bMOUNT%b ventoy folder %b%s%b\n" \
+        "$bold" "$blue" "$reset" "$bold" "$reset" "$bold" "$reset" \
+        "$cyan" "$mnt" "$reset" \
     && $auth mkdir --parents "$mnt" \
     && $auth mount --options rw,umask=0000 "/dev/disk/by-label/$label" "$mnt"
 
 # copy klassiker theme files
 [ -d "$mnt/ventoy" ] \
-    && printf ":: remove theme files from %s\n" "$mnt" \
+    && printf "%b%b::%b %bREMOVE%b theme files from %b%s%b\n" \
+        "$bold" "$blue" "$reset" "$bold" "$reset" "$cyan" "$mnt" "$reset" \
     && rm --recursive --force "$mnt/ventoy"
 [ -d "$mnt" ] \
-    && printf ":: copy klassiker theme files to %s\n" "$mnt" \
+    && printf "%b%b::%b %bCOPY%b klassiker theme files to %b%s%b\n" \
+        "$bold" "$blue" "$reset" "$bold" "$reset" "$cyan" "$mnt" "$reset" \
     && ! [ -d "$mnt/ventoy" ] \
     && cp --recursive "ventoy" "$mnt/ventoy"
 
 # create folder structure for klassiker theme
 [ -d "$mnt" ] \
-    && printf ":: create folder structure for klassiker theme in %s\n" "$mnt" \
+    && printf "%b%b::%b %bCREATE%b folder structure for klassiker theme in %b%s%b\n" \
+        "$bold" "$blue" "$reset" "$bold" "$reset" "$cyan" "$mnt" "$reset" \
     && mkdir --parents \
         "$mnt/data" \
         "$mnt/isos/diagnostic" \
@@ -36,12 +47,14 @@ label="Ventoy"
         "$mnt/isos/linux" \
         "$mnt/isos/pentest" \
         "$mnt/isos/rescue" \
+        "$mnt/isos/security" \
         "$mnt/isos/windows"
 
 # create checksum file with example iso
 [ -d "$mnt" ] \
     && ! [ -e "$mnt/ventoy_checksum" ] \
-    && printf ":: create ventoy checksum file in %s\n" "$mnt" \
+    && printf "%b%b::%b %bCREATE%b ventoy checksum file in %b%s%b\n" \
+        "$bold" "$blue" "$reset" "$bold" "$reset" "$cyan" "$mnt" "$reset" \
     && printf "%b" \
         "# diagnostic\n\n" \
         "# firmware\n\n" \
@@ -50,10 +63,13 @@ label="Ventoy"
         "isos/linux/archlinux-2024.04.01-x86_64.iso\n\n" \
         "# pentest\n\n" \
         "# rescue\n\n" \
+        "# security\n\n" \
         "# windows" > "$mnt/ventoy_checksum"
 
 # unmount ventoy partition
 [ -d "$mnt" ] \
-    && printf ":: unmount and delete ventoy folder %s\n" "$mnt" \
+    && printf "%b%b::%b %bUNMOUNT%b and %bDELETE%b ventoy folder %b%s%b\n" \
+        "$bold" "$blue" "$reset" "$bold" "$reset" "$bold" "$reset" \
+        "$cyan" "$mnt" "$reset" \
     && $auth umount "$mnt" \
     && $auth find "$mnt" -empty -type d -delete
